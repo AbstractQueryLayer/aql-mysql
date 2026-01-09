e9367992beec230d298d95f0708bac14125650ad
Edmond <1571649+edmonddantes@users.noreply.github.com>
2026-01-09 16:21:43 +0000

    Update SqlString namespace to use test-utils

--- tests/Ddl/Parser/AlterTableTest.php
+++ tests/Ddl/Parser/AlterTableTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace IfCastle\AQL\MySql\Ddl\Parser;
 
-use IfCastle\AQL\Dsl\Parser\SqlString;
+use IfCastle\AQL\TestCases\SqlString;
 use PHPUnit\Framework\TestCase;
 
 class AlterTableTest extends TestCase
